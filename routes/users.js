e411fa37e277cd200cf888fc4110d45c52cf2404
vskvj3 <visakhvj3@gmail.com>
2023-01-07 22:27:02 +0530

    added routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ import express from "express";
 import { v4 as uuidv4 } from 'uuid';
 import {getUser, createUser, deleteUser, updateUser, getUserWithID} from '../controllers/users.js'
 
-
 const router = express.Router();
 
 // a mock database
